c624109fd1708af91a92b20e776f7c2e4d360102
Andreas Hultgren <me@andreashultgren.se>
2020-03-02 15:38:56 +0530

    Hide navbar in app mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
       {
         hid: 'description',
         name: 'description',

00030c024213cc86b967b84b43781421e75c9a31
Andreas Hultgren <me@andreashultgren.se>
2022-01-16 14:58:16 +0100

    Migrate data from old domain

---
+++ pages/export.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Exporting...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    if (window.parent) {
+      console.log('POST')
+      window.parent.postMessage(localStorage.periods, '*')
+    }
+  }
+}
+</script>

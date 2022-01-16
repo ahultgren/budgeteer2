22bf5f361f64508a807b51c989bf4e1aa4920546
Andreas Hultgren <me@andreashultgren.se>
2022-01-16 14:58:16 +0100

    Migrate data from old domain

---
+++ pages/migrate.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <h1>Import data from herkou</h1>
+    <iframe src="https://ahbudgeteer.herokuapp.com" height="0" />
+
+    <div v-if="importedData">
+      The following data was found on the old domain. Want to import it?
+      <button @click="doImport()">Yes!</button>
+      <p>Then go <nuxt-link to="/">here</nuxt-link> to use the app</p>
+      <p>{{ importedData }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      importedData: ''
+    }
+  },
+  mounted() {
+    console.log('Parent mounted')
+    window.addEventListener(
+      'message',
+      (msg) => {
+        console.log('MESSAGE', msg)
+
+        if (msg.data[0] === '[') {
+          this.importedData = msg.data
+        }
+      },
+      false
+    )
+  },
+  methods: {
+    doImport() {
+      localStorage.periods = this.importedData
+      console.log('DONE')
+    }
+  }
+}
+</script>
+
+<style lang="less"></style>
